65078dc467060e79e885ca67fdd531df6cd589a7
Olivier Tilloy <olivier.tilloy@canonical.com>
2018-03-23 09:35:01 +0100

    Refactor custom plugins to make them inherit the corresponding snapcraft plugins.

--- snap/plugins/x-autotools.py
+++ snap/plugins/x-autotools.py
@@ -1,6 +1,6 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 #
-# Copyright (C) 2015-2016 Canonical Ltd
+# Copyright (C) 2015-2018 Canonical Ltd
 # Copyright (C) 2016 Harald Sitter <sitter@kde.org>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -15,79 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""The autotools plugin is used for autotools based parts.
-
-Autotools based projects are the ones that have the usual
-`./configure && make && make install` instruction set.
-
-The plugin tries to build using ./configure first, if it is not there
-it will run ./autogen and if autogen is not there it will run autoreconf.
-
-This plugin uses the common plugin keywords as well as those for "sources".
-For more information check the 'plugins' topic for the former and the
-'sources' topic for the latter.
-
-In addition, this plugin uses the following plugin-specific keywords:
-
-    - configflags:
-      (list of strings)
-      configure flags to pass to the build such as those shown by running
-      './configure --help'
-    - install-via:
-      (enum, 'destdir' or 'prefix')
-      Whether to install via DESTDIR or by using --prefix (default is
-      'destdir')
-"""
-
 import os
 import stat
 
-from snapcraft.plugins import make
+from snapcraft.plugins import autotools
 
 
-class AutotoolsPlugin(make.MakePlugin):
-
-    @classmethod
-    def schema(cls):
-        schema = super().schema()
-        schema['properties']['configflags'] = {
-            'type': 'array',
-            'minitems': 1,
-            'items': {
-                'type': 'string',
-            },
-            'default': [],
-        }
-
-        schema['properties']['install-via'] = {
-            'enum': ['destdir', 'prefix'],
-            'default': 'destdir',
-        }
-
-        return schema
-
-    @classmethod
-    def get_build_properties(cls):
-        # Inform Snapcraft of the properties associated with building. If these
-        # change in the YAML Snapcraft will consider the build step dirty.
-        return super().get_build_properties() + ['configflags', 'install-via']
-
-    def __init__(self, name, options, project):
-        super().__init__(name, options, project)
-        self.build_packages.extend([
-            'autoconf',
-            'automake',
-            'autopoint',
-            'libtool',
-        ])
-
-        if options.install_via == 'destdir':
-            self.options.make_install_var = 'DESTDIR'
-        elif options.install_via == 'prefix':
-            self.options.make_install_var = ''
-        else:
-            raise RuntimeError('Unsupported installation method: "{}"'.format(
-                options.install_via))
+class XAutotoolsPlugin(autotools.AutotoolsPlugin):
 
     def env(self, root):
         env = super().env(root)
@@ -98,21 +32,30 @@ class AutotoolsPlugin(make.MakePlugin):
             ])
         return env
 
-    def enable_cross_compilation(self):
-        pass
-
     def build(self):
         env = os.environ.copy()
-        env['XDG_DATA_DIRS'] = self.project.stage_dir + '/usr/share:/usr/share'
-        env['PKG_CONFIG_PATH'] = os.path.join(self.project.stage_dir, 'pkgconfig-build')
-        env['ACLOCAL_PATH'] = os.path.join(self.project.stage_dir, 'usr', 'share', 'aclocal')
+        stage_dir = self.project.stage_dir
+        env['XDG_DATA_DIRS'] = ':'.join([
+            os.path.join(stage_dir, 'usr/share'),
+            '/usr/share'])
+        env['PKG_CONFIG_PATH'] = os.path.join(stage_dir, 'pkgconfig-build')
+        env['ACLOCAL_PATH'] = os.path.join(stage_dir, 'usr/share/aclocal')
         if self.name != 'vala':
             # The vala part is a special case, it boostraps itself using the
-            # vala compiler on the host system and vapi files in the source tree
-            env['GI_TYPELIB_PATH'] = self.project.stage_dir + '/usr/lib/' + self.project.arch_triplet + '/girepository-1.0:/usr/lib/' + self.project.arch_triplet + '/girepository-1.0'
-            env['VAPIDIR'] = self.project.stage_dir + '/usr/share/vala/vapi:' + self.project.stage_dir + '/usr/share/vala-0.40/vapi:/usr/share/vala-0.30/vapi/'
-            env['VALAFLAGS'] = '--vapidir ' + self.project.stage_dir + '/usr/share/vala/vapi --vapidir ' + self.project.stage_dir + '/usr/share/vala-0.40/vapi --vapidir /usr/share/vala-0.30/vapi'
-            env['LD_LIBRARY_PATH'] = self.project.stage_dir + '/usr/lib/vala-0.40'
+            # vala compiler on the host system and vapi files in the source
+            # tree
+            arch = self.project.arch_triplet
+            env['GI_TYPELIB_PATH'] = ':'.join([
+                os.path.join(stage_dir, 'usr/lib', arch, 'girepository-1.0'),
+                os.path.join('/usr/lib', arch, 'girepository-1.0')])
+            vapidirs = [
+                os.path.join(stage_dir, 'usr/share/vala/vapi'),
+                os.path.join(stage_dir, 'usr/share/vala-0.40/vapi'),
+                '/usr/share/vala-0.30/vapi']
+            env['VAPIDIR'] = ':'.join(vapidirs)
+            env['VALAFLAGS'] = ' '.join(['--vapidir ' + v for v in vapidirs])
+            env['LD_LIBRARY_PATH'] = \
+                os.path.join(stage_dir, 'usr/lib/vala-0.40')
 
         if not os.path.exists(os.path.join(self.builddir, 'configure')):
             generated = False
@@ -127,7 +70,8 @@ class AutotoolsPlugin(make.MakePlugin):
                              stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
                              stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP |
                              stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH)
-                self.run(['env', 'NOCONFIGURE=1', './{}'.format(script)], env=env)
+                self.run(
+                    ['env', 'NOCONFIGURE=1', './{}'.format(script)], env=env)
                 generated = True
                 break
             if not generated:
@@ -144,10 +88,3 @@ class AutotoolsPlugin(make.MakePlugin):
             configure_command.append('--host={}'.format(self.project.deb_arch))
         self.run(configure_command + self.options.configflags, env=env)
         self.make(env=env)
-
-    def snap_fileset(self):
-        fileset = super().snap_fileset()
-        # Remove .la files which don't work when they are moved around
-        fileset.append('-**/*.la')
-        return fileset
-
