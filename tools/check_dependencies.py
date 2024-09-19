7f2789a0d6130b029b43dfa818f4652a6c9f48ee
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2024-09-19 13:55:01 +0200

    Add missing dependency and tool for checking
    There is a missing dependency in DEBS part: webp-pixbuf-loader.
    This patch adds it and also adds a check to ensure that any
    push fulfills the dependencies, ensuring that DEBS is ran after
    all the previous parts.

---
+++ tools/check_dependencies.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import yaml
+
+# Ensure that "deb" part depends on all previous parts
+
+config_file = "snapcraft.yaml"
+if not os.path.exists(config_file):
+    config_file = os.path.join("snap", "snapcraft.yaml")
+
+data = yaml.safe_load(open(config_file, "r"))
+
+parts = {}
+exceptions = ['buildenv']
+
+for part in data['parts']:
+    if part == "debs":
+        break
+    if part in exceptions:
+        continue
+    parts[part] = False
+
+def filldeps(part):
+    global data
+    global parts
+    if 'after' not in data['parts'][part]:
+        return
+
+    deps = data['parts'][part]['after']
+    for dep in deps:
+        parts[dep] = True
+        filldeps(dep)
+
+filldeps("debs")
+failed = False
+for part in parts:
+    if parts[part]:
+        continue
+    print(f"DEBS must depend on {part}")
+    failed = True
+
+if failed:
+    sys.exit(1)
+print("All dependencies are correct")
