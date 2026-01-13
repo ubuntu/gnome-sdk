d575969d17173edac22d009e30464c3728f7c558
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2026-01-13 17:55:54 +0100

    Some nitpicks

--- local-build.py
+++ local-build.py
@@ -38,7 +38,10 @@ gnome_part = config['parts']['gnome-sdk']
 del gnome_part['stage-snaps']
 
 version = config['name'].split('-')[1]
-gnome_part['build-environment'] = [{'LOCAL_SDK_SNAP': SDK_FILE}, {'sdk_version': version}]
+gnome_part['build-environment'] = [
+    {'LOCAL_SDK_SNAP': SDK_FILE},
+    {'sdk_version': version}
+]
 
 with open(f'./{MODIFIED_CONFIG}', "w") as config_file:
     config_file.write(yaml.dump(config, Dumper=yaml.Dumper))
