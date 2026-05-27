7a7760352288805913918193497cadcf3aef448d
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2026-05-27 12:47:38 +0200

    Allow to build Runtime from custom SDK
    This patch adapts the script from gnome-46-2404 that allows to
    build the Runtime from a custom, prebuilt SDK. It is needed to
    be able to test any PR in the SDK using CI.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-git clone -b gnome-42-2204 https://github.com/ubuntu/gnome-sdk.git gnome-42-2204
+# GNOME-42-2204 runtime
+
+This snap builds the GNOME-42-2204 runtime from the corresponding GNOME-42-2204-SDK
+snap.
+
+By default, it will get the SDK from the store, from the CANDIDATE branch; but it is
+possible to use a different SDK (for example, one built locally), by creating a folder
+called `base-gnome-sdk`, putting it inside, and launching the
+`local-build.py` script. It will create a new, modified `snapcraft.yaml` file in the,
+project's root, clean the snapcraft build environment, build the new snap, and restore
+the `snapcraft.yaml` file. This is useful if you do a change in the SDK and want
+to test it, to ensure that everything works as expected and nothing breaks.
+
+If the script finds several `snap` files inside the `base-gnome-sdk` folder, it will
+use the most recent one, based on the modification time of the file.
+
+Also, if the build must be done by an external script (like when using Github's CI),
+then it is possible to call the `local-build.py` script with the `--prepare-only`
+parameter. With it, it will just generate the modified `snapcraft.yaml` file in the
+project folder, nothing else.
+
+## Getting the repository
+
+To get the repository, just run:
+
+    git clone -b gnome-42-2204 https://github.com/ubuntu/gnome-sdk.git gnome-42-2204
 
 This branch is used to build [the gnome-42-2204 snap](https://launchpad.net/~desktop-snappers/+snap/gnome-42-2204),
 which in turn uses the [the gnome-42-2204-sdk snap](https://launchpad.net/~desktop-snappers/+snap/gnome-42-2204-sdk).
---
+++ local-build.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# This script builds the gnome runtime snap using a local SDK snap.
+
+import sys
+import os
+import subprocess
+import yaml
+import glob
+
+def str_presenter(dumper, data):
+    """configures yaml for dumping multiline strings
+    Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+    Ensures that the scripts are dumped in the right multiline format """
+    if len(data.splitlines()) > 1:  # check for multiline string
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+yaml.add_representer(str, str_presenter)
+yaml.representer.SafeRepresenter.add_representer(str, str_presenter) # to use with safe_dum
+
+BASE_CONFIG = 'snap/snapcraft.yaml'
+MODIFIED_CONFIG = 'snapcraft.yaml'
+BASE_SNAP_FOLDER = 'base-gnome-sdk'
+SDK_FILE = None
+PREPARE_ONLY_OPTION = '--prepare-only'
+
+# If there is a modified snapcraft.yaml, delete it before re-generating it
+if os.path.exists(f'./{MODIFIED_CONFIG}'):
+    os.remove(f'./{MODIFIED_CONFIG}')
+
+last_time = None
+# If there are more than one snap, use the most recent one
+for fullpath in glob.glob(os.path.join(BASE_SNAP_FOLDER, "*.snap")):
+    now_time = os.path.getmtime(fullpath)
+    if (last_time is None) or (now_time > last_time):
+        SDK_FILE = fullpath
+        last_time = now_time
+
+if SDK_FILE is None:
+    print(f'There is no valid SDK file in the {BASE_SNAP_FOLDER} folder. Aborting.', file=sys.stderr)
+    sys.exit(1)
+
+with open(f'./{BASE_CONFIG}', "r") as config_file:
+    config = yaml.load(config_file, Loader=yaml.Loader)
+
+gnome_part = config['parts']['gnome-sdk']
+
+# remove the stage-snaps entry
+del gnome_part['stage-snaps']
+
+version = config['name'].split('-')[1]
+gnome_part['build-environment'] = gnome_part.get('build-environment', []) + [
+    {'LOCAL_SDK_SNAP': SDK_FILE},
+    {'sdk_version': version}
+]
+
+try:
+    with open(f'./{MODIFIED_CONFIG}', "w") as config_file:
+        config_file.write(yaml.dump(config, Dumper=yaml.Dumper))
+    if PREPARE_ONLY_OPTION not in sys.argv:
+        subprocess.call('snapcraft clean', shell=True)
+        subprocess.call('snapcraft pack -v', shell=True)
+finally:
+    if PREPARE_ONLY_OPTION not in sys.argv:
+        os.remove(f'./{MODIFIED_CONFIG}')
